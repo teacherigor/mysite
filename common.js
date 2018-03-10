efdb6d430d2cb7f788caff5a583987fbbd93b814
teacherigor <teacherigor6@gmail.com>
2018-03-10 17:18:50 +0200

    var vocabularyData

---
+++ common.js
@@ -0,0 +1,9 @@
+var vocabularyData = [
+	{
+		id: 0,
+		question: "This program is full of top tips to help you learn useful business English.",
+		answers: ["useless", "horrible", "best"],
+		rightAnswer: "best"
+	}
+];
+console.log(vocabularyData);
