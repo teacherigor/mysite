a9bdd7d1a61e6b6c71c3375875ed91755af88949
teacherigor <teacherigor6@gmail.com>
2018-04-22 21:36:34 +0300

    question + break

--- common.js
+++ common.js
@@ -28,6 +28,6 @@ for (var questionIndex = 0; questionIndex < vocabularyData.length; questionIndex
 
 function getQuestionBox(question) {
 	var box = document.createElement('div');
-	document.write(question);
+	document.write(question + "<br/>");
 	return box;
 }
