ecfd2be07fa038ef7b47b2a635e8dbb47a70521b
teacherigor <teacherigor6@gmail.com>
2018-04-29 16:24:53 +0300

    node.className = question

--- common.js
+++ common.js
@@ -1,18 +1,18 @@
 var vocabularyData = [
 	{
-		id: 0,
+		id: 'asdr-3432-asds',
 		question: "This program is full of top tips to help you learn useful business English.",
 		answers: ["useless", "horrible", "best"],
 		rightAnswer: "best"
 	},
 	{
-		id: 1,
+		id: 'asdr-dfde-asds',
 		question: "Anna will be an asset for the Tip Top Trading.",
 		answers: ["valuable employee", "disorganised employee", "immature employee"],
 		rightAnswer: "valuable employee"
 	},
 	{
-		id: 2,
+		id: 'asd2-3we2-asds',
 		question: "When things go wrong, Paul has to take charge.",
 		answers: ["a coffee break", "sick leave", "responsibility"],
 		rightAnswer: "responsibility"
@@ -22,12 +22,16 @@ var vocabularyData = [
 var questionsForm = document.getElementById('questionslist');
 
 for (var questionIndex = 0; questionIndex < vocabularyData.length; questionIndex++) {
-	questionsForm.appendChild(getQuestionBox(vocabularyData[questionIndex].question));
+	var node = document.createElement('div');
+	node.innerHTML = vocabularyData[questionIndex].question;
+	node.className = 'question';
+	questionsForm.appendChild(node);
+
 }
 
 
-function getQuestionBox(question) {
-	var box = document.createElement('div');
-	box.appendChild('question');
-	document.getElementById('questionslist'). appendChild(box);
-	}
+// function getQuestionBox(question) {
+// 	var box = document.createElement('div');
+// 	box.appendChild('question');
+// 	document.getElementById('questionslist'). appendChild(box);
+// 	}
